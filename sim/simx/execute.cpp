c6020ac4b06286801c9f3d750b1e3eac571e2dd8
ArunanElamaran <arunan.elamaran@gmail.com>
2025-12-05 16:25:33 -0800

    Framework for decode and execute added BUT need to make specific for UMMA

--- sim/simx/execute.cpp
+++ sim/simx/execute.cpp
@@ -1479,6 +1479,13 @@ instr_trace_t* Emulator::execute(const Instr &instr, uint32_t wid) {
         tensor_unit_->wmma(wid, tpuArgs.fmt_s, tpuArgs.fmt_d, tpuArgs.step_m, tpuArgs.step_n, rs1_data, rs2_data, rs3_data, rd_data, trace_data.get());
         rd_write = true;
       } break;
+      case TcuType::UMMA: {
+        auto trace_data = std::make_shared<TensorUnit::ExeTraceData>();
+        trace->data = trace_data;
+        assert(warp.tmask.count() == num_threads);
+        tensor_unit_->wmma(wid, tpuArgs.fmt_s, tpuArgs.fmt_d, tpuArgs.step_m, tpuArgs.step_n, rs1_data, rs2_data, rs3_data, rd_data, trace_data.get());
+        rd_write = true;
+      } break;
       default:
         std::abort();
       }
